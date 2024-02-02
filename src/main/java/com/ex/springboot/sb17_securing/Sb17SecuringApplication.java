cdfe311a69be5506354cbb87d5b73bcad6258699
tester <osharthur112@naver.com>
2024-02-02 10:02:46 +0900

    Changes

---
+++ src/main/java/com/ex/springboot/sb17_securing/Sb17SecuringApplication.java
@@ -0,0 +1,13 @@
+package com.ex.springboot.sb17_securing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb17SecuringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb17SecuringApplication.class, args);
+    }
+
+}
